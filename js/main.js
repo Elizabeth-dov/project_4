54593cfe586c16bc46a7e4bc7eb26376e825e729
Elizabeth <142344859+Elizabeth-dov@users.noreply.github.com>
2024-07-15 16:42:07 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.projects__slider', {
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true 
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  const menuBtn = document.querySelector('.menu__btn');
+const menuClose = document.querySelector('.menu__close');
+const menuList = document.querySelector('.menu__list');
+const menuShadow = document.querySelector('.menu__close')
+
+
+menuBtn.addEventListener('click', ()=>{
+    menuList.classList.toggle('menu__list--open');
+    menuShadow.classList.toggle('menu--open');
+});
+menuClose.addEventListener('click', ()=> {
+    menuList.classList.remove('menu__list--open');
+    menuShadow.classList.remove('menu--open');
+});
